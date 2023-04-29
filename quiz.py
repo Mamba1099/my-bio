84c43f3988f78479b24e8a7881279ccda008e81f
Mamba1099 <mambakaranja240@gmail.com>
2023-04-29 21:07:32 +0300

    questionnaires

---
+++ quiz.py
@@ -0,0 +1,27 @@
+def my_name(name):
+    print("Hi" + " " +name)
+
+my_name(input("enter your name:"))
+
+
+def my_colour(colour):
+    print("Your favorite colour is" + " " + colour)
+
+my_colour(input("Enter your favorite color:\n"))
+
+
+def my_age(age):
+    print("You are" + " " + age)
+
+my_age(input("Enter your age:\n"))
+
+
+def my_country(country):
+    print("You live in" + " " + country)
+
+my_country(input("Enter the country you live in:\n"))
+
+print("Thank you")
+
+
+
